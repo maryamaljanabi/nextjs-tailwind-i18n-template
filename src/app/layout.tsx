1cc030468efe8044cdffc1f3694cc7102bf2fd94
maryamaljanabi <maryam.aljanabi135@gmail.com>
2025-08-30 11:25:13 +0300

    init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import { Open_Sans, Roboto_Mono } from "next/font/google";
+import "./globals.css";
+
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html>
+      <body
+        className={`${openSans.variable} ${robotoMono.variable} overflow-x-hidden antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 text-justify">
+      Template
+    </main>
+  );
+}
